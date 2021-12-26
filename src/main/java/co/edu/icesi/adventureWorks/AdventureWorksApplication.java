947324222ceaa81a8cf0508f56fb8ebbb058d006
Felipe Gómez <luis.andrade18@hotmail.com>
2021-12-26 17:24:49 -0500

    first commit

---
+++ src/main/java/co/edu/icesi/adventureWorks/AdventureWorksApplication.java
@@ -0,0 +1,13 @@
+package co.edu.icesi.adventureWorks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventureWorksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdventureWorksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/icesi/adventureWorks/AdventureWorksApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.icesi.adventureWorks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventureWorksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
