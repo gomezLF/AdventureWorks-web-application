ee1c2205023ee7102ff0e6a15cc783babe8338cf
Felipe Gómez <luis.andrade18@hotmail.com>
2021-12-27 16:56:20 -0500

    Added two new attributes: rowguid and modifiedDate with their getters-setters methods

--- src/main/java/co/edu/icesi/adventureWorks/model/Person/Contact.java
+++ src/main/java/co/edu/icesi/adventureWorks/model/Person/Contact.java
@@ -1,5 +1,6 @@
 package co.edu.icesi.adventureWorks.model.Person;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import javax.persistence.Entity;
@@ -26,25 +27,30 @@ public class Contact {
 	 */
 	private String title;
 	
+	
 	/**
 	 * First name of the person.
 	 */
 	private String firstName;
+	
 	
 	/**
 	 * Middle name or middle initial of the person.
 	 */
 	private String middleName;
 	
+	
 	/**
 	 * Last name of the person.
 	 */
 	private String lastName;
 	
+	
 	/**
 	 * Surname suffix. For example, Sr. or Jr.
 	 */
 	private String suffix;
+	
 	
 	/**
 	 * False = Contact does not wish to receive e-mail promotions.
@@ -52,23 +58,39 @@ public class Contact {
 	 */
 	private Boolean emailPromotion;
 	
+	
 	/**
 	 * Indicates the column in which the family name is stored.
 	 * 
 	 * 0 = Family name is stored in LastName and the first name is stored in FirstName.
 	 * 1 = .Family name is stored in FirstName and the first name is stored in LastName.
 	 */
-	private NameStyle nameStyle;
+	private Integer nameStyle;
+	
+	
+	/**
+	 * ROWGUIDCOL number uniquely identifying the row. Used to support a merge replication sample.
+	 */
+	private Integer rowguid;
+	
+	
+	/**
+	 * Date and time the row was last updated.
+	 */
+	private LocalDateTime modifiedDate;
+	
 	
 	/**
 	 * E-mail addresses for the person.
 	 */
 	private List<EmailAddress> emailAddresses;
 	
+	
 	/**
 	 * Phone number associated with the person.
 	 */
 	private List<Phone> phones;
+	
 	
 	/**
 	 * Password for the e-mail account.
@@ -80,9 +102,9 @@ public class Contact {
 	public Contact() {
 		
 	}
-
-
-
+	
+	
+	
 	public Integer getContactID() {
 		return contactID;
 	}
@@ -111,8 +133,16 @@ public class Contact {
 		return emailPromotion;
 	}
 
-	public NameStyle getNameStyle() {
+	public Integer getNameStyle() {
 		return nameStyle;
+	}
+	
+	public Integer getRowguid() {
+		return rowguid;
+	}
+	
+	public LocalDateTime getModifiedDate() {
+		return modifiedDate;
 	}
 
 	public List<EmailAddress> getEmailAddresses() {
@@ -125,6 +155,60 @@ public class Contact {
 
 	public Password getPassword() {
 		return password;
+	}
+
+
+
+	public void setContactID(Integer contactID) {
+		this.contactID = contactID;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setSuffix(String suffix) {
+		this.suffix = suffix;
+	}
+
+	public void setEmailPromotion(Boolean emailPromotion) {
+		this.emailPromotion = emailPromotion;
+	}
+
+	public void setNameStyle(Integer nameStyle) {
+		this.nameStyle = nameStyle;
+	}
+	
+	public void setRowguid(Integer rowguid) {
+		this.rowguid = rowguid;
+	}
+	
+	public void setModifiedDate(LocalDateTime modifiedDate) {
+		this.modifiedDate = modifiedDate;
+	}
+
+	public void setEmailAddresses(List<EmailAddress> emailAddresses) {
+		this.emailAddresses = emailAddresses;
+	}
+
+	public void setPhones(List<Phone> phones) {
+		this.phones = phones;
+	}
+
+	public void setPassword(Password password) {
+		this.password = password;
 	}
 	
 	
