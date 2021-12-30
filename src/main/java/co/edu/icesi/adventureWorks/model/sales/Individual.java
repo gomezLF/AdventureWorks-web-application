45d9efb17dbd8495e38626bc38cab755bbf82301
Felipe Gómez <luis.andrade18@hotmail.com>
2021-12-30 17:32:54 -0500

    Created the Individual class with all its attributes.

---
+++ src/main/java/co/edu/icesi/adventureWorks/model/sales/Individual.java
@@ -0,0 +1,71 @@
+package co.edu.icesi.adventureWorks.model.sales;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.SequenceGenerator;
+
+import co.edu.icesi.adventureWorks.model.Person.Contact;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@NamedQuery(name = "Individual.findAll", query = "SELECT i FROM Individual i")
+public class Individual implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Unique identifier of the individual.
+	 */
+	@Id
+	@SequenceGenerator(name = "INDIVIDUAL_INDIVIDUALID_GENERATOR", allocationSize = 1, sequenceName = "INDIVIDUAL_SEQ")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "INDIVIDUAL_INDIVIDUALID_GENERATOR")
+	@Getter
+	@Setter
+	private Integer individualID;
+	
+	
+	/**
+	 * Personal information, such as hobbies and income that is collected from online shoppers and used for sales analysis.
+	 */
+	@Getter
+	@Setter
+	private String demographic;
+	
+	
+	/**
+	 * Date the individual was last edited.
+	 */
+	@Getter
+	@Setter
+	private LocalDateTime  modifiedDate;
+	
+	
+	/**
+	 * Identifies the customer in the Contact table.
+	 */
+	@Getter
+	@Setter
+	private Contact contact;
+	
+	
+	/**
+	 * Unique customer identification number. 
+	 */
+	@Getter
+	@Setter
+	private Customer customer;
+	
+	
+	
+	public Individual() {
+		setModifiedDate(LocalDateTime.now());
+	}
+	
+}
