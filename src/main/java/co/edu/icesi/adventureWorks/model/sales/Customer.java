59a85757038dbae63f1911486e00d4adc568d570
Felipe Gómez <luis.andrade18@hotmail.com>
2021-12-30 16:24:02 -0500

    Created the Customer class with all its attributes.

---
+++ src/main/java/co/edu/icesi/adventureWorks/model/sales/Customer.java
@@ -0,0 +1,74 @@
+package co.edu.icesi.adventureWorks.model.sales;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQuery;
+import javax.persistence.SequenceGenerator;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@NamedQuery(name = "Customer.findAll", query = "SELECT c FROM Customer c")
+public class Customer implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	
+	/**
+	 * Primary key for Customer rows.
+	 */
+	@Id
+	@SequenceGenerator(name = "CUSTOMER_CUSTOMERID_GENERATOR", allocationSize = 1, sequenceName = "CUSTOMER_SEQ")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "CUSTOMER_CUSTOMERID_GENERATOR")
+	@Getter
+	@Setter
+	private Integer customerID;
+	
+	
+	/**
+	 * Unique number that identifies the customer.
+	 */
+	@Getter
+	@Setter
+	private Integer accountNumber;
+	
+	
+	/**
+	 * Customer type. It can be an individual or a store. 
+	 */
+	@Getter
+	@Setter
+	private CustomerType customerType;
+	
+	
+	/**
+	 * Date the customer was last edited.
+	 */
+	@Getter
+	@Setter
+	private LocalDateTime modifiedDate;
+	
+	
+	/**
+	 * Territory in which the customer is located.
+	 */
+	@ManyToOne
+	@JoinColumn(name = "territoryid")
+	@Getter
+	@Setter
+	private SalesTerritory salesterritory;
+	
+	
+	
+	public Customer() {
+		setModifiedDate(LocalDateTime.now());
+	}
+}
