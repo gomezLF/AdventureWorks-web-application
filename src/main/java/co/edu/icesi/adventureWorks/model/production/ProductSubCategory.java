0ca2aa812e59096731637ce4eae6cf5536d89b29
Felipe Gómez <luis.andrade18@hotmail.com>
2022-01-10 17:45:01 -0500

    Created the ProductSubCategory with all its attributes and relationships with its add methods.

---
+++ src/main/java/co/edu/icesi/adventureWorks/model/production/ProductSubCategory.java
@@ -0,0 +1,90 @@
+package co.edu.icesi.adventureWorks.model.production;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@NamedQuery(name = "ProductSubcategory.findAll", query = "SELECT p FROM ProductSubcategory p")
+public class ProductSubCategory implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	
+	/**
+	 * Primary key for ProductSubcategory rows.
+	 */
+	@Id
+	@SequenceGenerator(name = "PRODUCTSUBCATEGORY_PRODUCTID_GENERATOR", allocationSize = 1, sequenceName = "PRODUCTSUBCATEGORY_SEQ")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "PRODUCTSUBCATEGORY_PRODUCTID_GENERATOR")
+	@Getter
+	@Setter
+	private Integer productSubcategoryID;
+	
+	
+	/**
+	 * Subcategory description.
+	 */
+	@Getter
+	@Setter
+	private String name;
+	
+	
+	/**
+	 * Date the product subcategory was last updated.
+	 */
+	@Getter
+	@Setter
+	private LocalDate modifiedDate;
+	
+	
+	/**
+	 * Product category to which this subcategory belongs.
+	 */
+	@Getter
+	@Setter
+	private ProductCategory productCategory;
+	
+	
+	/**
+	 * List of all products belonging to this subcategory.
+	 */
+	@OneToMany(mappedBy = "productsubcategory")
+	@Getter
+	@Setter
+	private List<Product> products;
+	
+	
+	
+	public ProductSubCategory() {
+		setProducts(new ArrayList<Product>());
+		setModifiedDate(LocalDate.now());
+	}
+	
+	
+	
+	/**
+	 * Adds a product, created prior to calling this function, to a product list. It then returns the added product.
+	 * @param product - The product to be added.
+	 * @return The product added. 
+	 */
+	public Product addProduct(Product product) {
+		getProducts().add(product);
+		product.setProductSubCategory(this);
+		
+		return product;
+	}
+	
+}
