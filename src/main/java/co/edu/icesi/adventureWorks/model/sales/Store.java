9bce704c2711272ce2893428917ddcb77861f817
Felipe Gómez <luis.andrade18@hotmail.com>
2022-02-23 11:09:22 -0500

    implemented both the addCustomer and addSalesPerson methods to add this objects to the lists

--- src/main/java/co/edu/icesi/adventureWorks/model/sales/Store.java
+++ src/main/java/co/edu/icesi/adventureWorks/model/sales/Store.java
@@ -6,7 +6,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -16,6 +22,16 @@ import lombok.Setter;
 public class Store implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Primary key for Store rows
+	 */
+	@Id
+	@SequenceGenerator(name = "STORE_STOREID_GENERATOR", allocationSize = 1, sequenceName = "STORE_SEQ")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "STORE_STOREID_GENERATOR")
+	@Getter
+	@Setter
+	private Integer storeID;
 	
 	/**
 	 * Name of the store.
@@ -42,24 +58,54 @@ public class Store implements Serializable{
 	
 	
 	/**
-	 * 
+	 * Customers who have purchased from the store.
 	 */
+	@ManyToMany(mappedBy = "stores")
 	@Getter
 	@Setter
-	private Customer customer;
+	private List<Customer> customers;
 	
 	
 	/**
-	 * 
+	 * Adventure Works sales representative responsible for selling products to the store.
 	 */
+	@OneToMany(mappedBy = "store")
 	@Getter
 	@Setter
-	private List<SalesPerson> salesPerson;
+	private List<SalesPerson> salesPersons;
 	
 	
 	
 	public Store() {
-		setSalesPerson(new ArrayList<SalesPerson>());
+		setCustomers(new ArrayList<Customer>());
+		setSalesPersons(new ArrayList<SalesPerson>());
 		setModifiedDate(LocalDate.now());
+	}
+	
+	
+	
+	/**
+	 * Adds a customer, created prior to calling this function, to the customer list. It then return the customer added.
+	 * @param customer - The customer to be added.
+	 * @return The customer added.
+	 */
+	public Customer addCustomer(Customer customer) {
+		getCustomers().add(customer);
+		customer.getStores().add(this);
+		
+		return customer;
+	}
+	
+	
+	/**
+	 * Adds a salesperson, created prior to calling this function, to the salesperson list. It then return the salesperson added.
+	 * @param salesPerson - The salesperson to be added.
+	 * @return The salesperson added.
+	 */
+	public SalesPerson addSalesPerson(SalesPerson salesPerson) {
+		getSalesPersons().add(salesPerson);
+		salesPerson.setStore(this);
+		
+		return salesPerson;
 	}
 }
